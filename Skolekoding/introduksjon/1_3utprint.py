61bb4d25b0c0cfa01f660714bf9fce60049a93cc
Skolekode <78789993+Skolekode@users.noreply.github.com>
2021-02-12 11:36:38 +0100

    Add files via upload

---
+++ Skolekoding/introduksjon/1_3utprint.py
@@ -0,0 +1,15 @@
+"""
+Når vi koder vil vi komme til å ønske å gjøre utprinter
+ofte og av forskjellige ting.
+Det vanligste vi gjør utprint av er:
+ - Rene tekststrenger
+ - Variabler
+ - kombinasjoner av disse
+
+#Eksempel på kompinasjon:
+sum = 200
+print("Du har:", sum, "igjen på kontoen din")
+
+legg merke til kommaene, de skiller ulike typer i print() funksjonen.
+kopier koden ut av kommentarfeltet, kjør og prøv å endre tekst og sum og kjør igjen
+"""
---
+++ Skolekoding/introduksjon/4_3kodeflyt.py
@@ -0,0 +1,22 @@
+"""
+I denne oppgaven lager vi en bankkonto.
+Kjøp på Vinslottet gjør at penger går ut.
+Utprinter gjør at vi stadig får et innblikk
+i hvordan bankkontoen ser ut.
+
+del 1
+Beskriv med en tegning hva som skjer i dette programmet
+og hva utprintene blir
+
+
+total_sum = int(input("Skriv en fiktiv sum på din bankkonto i heltall\n"))
+
+bank_oppdatering = "Din gjenværende sum er:"
+print(bank_oppdatering, total_sum)
+
+kjop_sum = total_sum / 3
+total_sum = total_sum - kjop_sum
+
+print(bank_oppdatering, total_sum)
+
+"""
