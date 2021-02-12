1edf7bacb5ca6ab6509ddb554a8298ee09f75a6c
Skolekode <78789993+Skolekode@users.noreply.github.com>
2021-02-12 12:30:39 +0100

    Add files via upload

---
+++ Skolekoding/introduksjon/2_5input.py
@@ -0,0 +1,16 @@
+"""
+Lag en kort adjektivfortelling som tar imot input fra bruker
+Hva er adjektiver?
+ - Beskrivende ord, eks. vakre, grønne, barske, stilfulle,
+
+ Eksempel på adjektivhistorie, kilde bursdagsjentene.no:
+
+ Nå sitter vi ved det ………. spisebordet til den ……….
+ familien til den ………. Henrik.
+ Han har invitert alle de ……….
+ vennene sine hit i dag bare fordi han fyller……….10 år.
+ Her sitter altså den ………. Erik  og han tror han er en ………. gutt.
+
+hint: du må ha like mange input-variabler som det er markeringer som ....
+      husk komma mellom setninger med tødler og variabler i print
+"""
