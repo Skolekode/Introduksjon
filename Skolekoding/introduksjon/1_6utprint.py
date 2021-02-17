9c3d67ed2cd5a1f912ee2f8b044227c17fbf1b2d
Skolekode <78789993+Skolekode@users.noreply.github.com>
2021-02-17 14:19:14 +0100

    Add files via upload

---
+++ Skolekoding/introduksjon/1_6utprint.py
@@ -0,0 +1,8 @@
+"""
+For å printe ut pent ønsker vi av og til å ha med linjeskift. Det lager vi
+med å kommandoen "\n".
+symbolet \ finner vi til høyre for + tegnet på tastaturet
+Eksempel:
+skriv kommandoen
+print("Hei\nPer")
+"""
---
+++ Skolekoding/introduksjon/1_7_utprint.py
@@ -0,0 +1,10 @@
+"""
+Vi kan bruke noen matematiske uttrykk på tekst-strenger, prøv disse selv.
+Tenk over hvorfor nøyaktiv dette ble resultatet.
+
+navn = "Kari"
+print(navn+navn+navn)
+print(navn*5)
+
+
+"""
